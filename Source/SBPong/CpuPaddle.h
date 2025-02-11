ba2ee40151f7d19897784bfffa5293859e36683c
Sean Bradbury <brad0488@algonquinlive.com>
2025-02-11 13:51:34 -0500

    second commit

---
+++ Source/SBPong/CpuPaddle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Paddle.h"
+#include "CpuPaddle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SBPONG_API ACpuPaddle : public APaddle
+{
+	GENERATED_BODY()
+	
+};
